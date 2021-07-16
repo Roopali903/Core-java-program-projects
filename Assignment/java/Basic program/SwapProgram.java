e7b806d0935fd3442a161cafce8c3844b3b14f4c
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-07-16 10:09:05 -0700

    Add files via upload

---
+++ Assignment/java/Basic program/SwapProgram.java
@@ -0,0 +1,21 @@
+import java.util.*;
+ class Swap
+{
+public static void main(String s[])
+{
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+System.out.println("enter first number");
+Scanner j = new Scanner(System.in);
+int b = j.nextInt();
+System.out.println("enter second number");
+int c ;
+ c = a;
+ a = b ;
+ b = c ;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}
+
