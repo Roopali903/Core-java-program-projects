bfba0db090545baa51efb7db4c05bb5113898cc7
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-07-16 09:23:20 -0700

    Add files via upload
    program to check if he/she eligible for vote or not

---
+++ Assignment/java/Basic program/Vote.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Year{
+public static void main(String s[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("enter your age: ");
+int a = sc.nextInt();
+if(a>=18)
+{
+System.out.println("eligible");
+}
+else{
+System.out.println("not eligible");
+}
+}
+}
+
+
+
+
