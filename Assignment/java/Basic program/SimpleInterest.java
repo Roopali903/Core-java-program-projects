62a218f574a8fb68566cd93c688a847fc0e7cf07
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-07-16 10:21:41 -0700

    Add files via upload

---
+++ Assignment/java/Basic program/SimpleInterest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class SimpleInterest{
+public static void main(String s[])
+{
+Scanner sc = new Scanner(System.in);
+float p = sc.nextFloat();
+float r = sc.nextFloat();
+float t = sc.nextFloat();
+ float si = (p*r*t)/100;
+
+System.out.println("Simple interest is:" +si);
+}
+}
+© 2021 GitHub, Inc.
