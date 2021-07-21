06c070afa7704651abede3fba892dfe09f6fb8fd
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-07-21 04:00:01 -0700

    Add files via upload

---
+++ Assignment/java/loops & array program/AvgSum.java
@@ -0,0 +1,18 @@
+class AvgSum{
+public static void main(String s[])
+{
+int a[] = {4 , 5, 7, 8};
+int sum = 0 , Average;
+{
+for(int i=0 ; i < a.length ; i++)
+{
+sum = sum + a[i];
+}
+Average = sum/a.length ; 
+System.out.println("the avg is : "+Average);
+}
+}
+}
+
+
+
