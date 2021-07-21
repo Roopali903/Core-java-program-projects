d2ac2545ee789c78fa1727869532c51ba1c97a87
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-07-21 03:58:24 -0700

    Add files via upload

---
+++ Assignment/java/loops & array program/AscDsc.java
@@ -0,0 +1,25 @@
+class AscDsc{
+public static void main(String s[])
+{
+int i;
+System.out.println("even number") ;
+for(i=0;i<100;i++)
+{
+{
+if(i%2==0)
+{
+
+System.out.println(i);
+}
+}
+System.out.println("descending ");
+for(int j=100 ; j>0; j--)
+{
+if(i%2 != 0)
+{
+System.out.println(j);
+}
+}
+}
+}
+}
