b8e4a2c86e605cb6fd16698691ec31748f481e5d
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-07-21 04:00:54 -0700

    Add files via upload

---
+++ FactOrial.java
@@ -0,0 +1,12 @@
+class FactOrial{
+public static void main(String s[])
+{
+int n = 5 , fact = 1,i=1;
+while(i<=n)
+{
+fact = fact * i;
+i++;
+}
+System.out.println("Factorial of a number :" +fact);
+}
+}
