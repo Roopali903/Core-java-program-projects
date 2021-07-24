c94b28c7e7ad4f641eef6a4e1e73ab290cbefe48
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-07-24 12:09:37 -0700

    Add files via upload

---
+++ Assignment/java/loops & array program/InsertArray.java
@@ -0,0 +1,23 @@
+
+class InsertArray{
+public static void main(String s[])
+{
+System.out.println("Enter the size of an array");
+int a[] = {2,3,9,5,0,1,6,8};
+for(int i=0;i<a.length;i++){
+for(int j=i;j<a.length;j++)
+{
+if(a[i]>a[j]){
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+
+}
+}
+}
+
+for(int i=0;i<a.length;i++){
+System.out.println(" "+a[i]+" ");
+}
+}
+}
