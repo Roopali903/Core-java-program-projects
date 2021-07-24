08ac973f481244ed704ab5511c020bc660d7c0d0
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-07-24 12:09:10 -0700

    Add files via upload

---
+++ Assignment/java/loops & array program/FindSpecificValue.java
@@ -0,0 +1,21 @@
+class FindSpecificValue{
+public static void main(String s[])
+{
+int a[] = {4 , 5, 7, 8};
+
+{
+for(int i=0 ; i < a.length ; i++)
+{
+if(arr[i]==n)
+{
+System.out.println("true");
+}
+else{
+System.out.println("false");
+}
+}
+}
+}
+
+
+
