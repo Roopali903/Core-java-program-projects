7b743c4362e63c741958bcb72670d679efad443f
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-07-24 12:21:48 -0700

    Add files via upload

---
+++ Assignment/java/loops & array program/SumOfArray.java
@@ -0,0 +1,16 @@
+class SumOfArray{
+public static void main(String s[])
+{
+int a[] = {2,4,8,9,3,60};
+int sum = 0;
+{
+for(int i=0 ; i<a.length ; i++)
+{
+
+sum = sum + a[i];
+}
+
+System.out.println("The sum is :"+sum);
+}
+}
+}
