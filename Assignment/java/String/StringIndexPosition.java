7415a10117c96a3973526a1ae439bbca437bf943
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-08-16 20:48:02 +0530

    Add files via upload

---
+++ Assignment/java/String/StringIndexPosition.java
@@ -0,0 +1,20 @@
+public class StringExampleIndexPositon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		
+  int arr[] = new int[8];
+  try {
+  for(int i=0 ; i<10 ;i++)
+  {
+  System.out.println(arr[i]);
+  System.out.println("your code is index out of bound");
+
+	}
+  }
+  catch(Exception e) {
+	  System.out.println(e);
+  }
+
+}
+}
