853974e9571c37966fdedc00c4a316c8c35eda72
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-08-16 20:41:40 +0530

    Add files via upload

---
+++ Assignment/java/String/findindexposition.java
@@ -0,0 +1,20 @@
+public class String1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Hello java";
+		System.out.println("my string: " +s);
+		int index1 = s.charAt(0);
+		int index2 = s.charAt(9);
+		int index3 = s.charAt(2);
+		int index4 = s.charAt(3);
+		int index5 = s.charAt(1);
+System.out.println("The char at position 0 is "+(char)index1);
+System.out.println("The char at position 9 is "+(char)index2);
+System.out.println("The char at position 2 is "+(char)(index3));
+System.out.println("The char at position 3 is "+(char)index4);
+System.out.println("The char at position 1 is "+(char)(index5));
+
+	}
+
+}
