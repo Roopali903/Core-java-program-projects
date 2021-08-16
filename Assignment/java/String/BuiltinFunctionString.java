59e592ade41fd654449631a2410e2155b3594e78
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-08-16 20:35:54 +0530

    Add files via upload

---
+++ Assignment/java/String/BuiltinFunctionString.java
@@ -0,0 +1,16 @@
+public class StringBuilt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "Selenium";
+		String str2 = "HEllO";
+		String str3 = "SELENIUM";
+		String str4 = "testing ";
+		System.out.println(str1.compareTo(str2));
+		System.out.println(str1.compareTo(str3));
+		System.out.println(str2.compareTo(str4));
+		
+
+	}
+
+}
