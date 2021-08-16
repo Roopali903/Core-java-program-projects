5ba384d1bfa18e21e0b74686d3da56b8ae96ec3b
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-08-16 20:37:14 +0530

    Add files via upload

---
+++ Assignment/java/String/checkIndexPosition.java
@@ -0,0 +1,14 @@
+public class StringExampleIndexPositon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "java exersise";
+		int index1 = s.charAt(0);
+		int index2 = s.charAt(5);
+		System.out.println("The index position 0 "+(char)index1);
+		System.out.println("The index position 5 "+(char)index2);
+		 
+
+	}
+
+}
