dfba90052ec2b0314b26e2333b04ad83b6a6a378
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-08-16 20:40:59 +0530

    Add files via upload

---
+++ Assignment/java/String/FindConstant.java
@@ -0,0 +1,15 @@
+package StringExample1;
+import java.util.*;
+
+public class StringExample1 {
+	
+
+	public static void main(String[] args) {
+		String s, s1;
+		Scanner sc = new Scanner(System.in);
+		s = sc.nextLine();
+		s1=s.replaceAll("[BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz]","");
+		System.out.println(s1);
+		}
+		 
+	}
