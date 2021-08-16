abeb6ebcdcab04adaf9d6c546a8b411920ca9b6d
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-08-16 20:47:02 +0530

    Add files via upload

---
+++ Assignment/java/String/StringConcat.java
@@ -0,0 +1,12 @@
+public class StringConcat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "java exersise";
+		
+  s1 = s1.concat(" hello java");
+  System.out.println(s1);
+
+	}
+
+}
