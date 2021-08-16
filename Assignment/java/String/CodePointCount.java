d89a0964fc522cad3e08b09f5b00267ddcb4a14f
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-08-16 20:38:49 +0530

    Add files via upload

---
+++ Assignment/java/String/CodePointCount.java
@@ -0,0 +1,13 @@
+public class String1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Learn programming";
+		System.out.println("my string: " +s);
+		int ctr = s.codePointCount(1,6);
+		
+System.out.println("codepoint count "+ ctr);
+
+	}
+
+}
