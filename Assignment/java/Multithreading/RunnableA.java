72c3043624178b1185854fc7a88368c37a904602
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-08-20 00:24:29 +0530

    Add files via upload

---
+++ Assignment/java/Multithreading/RunnableA.java
@@ -0,0 +1,42 @@
+public class A {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Runnable r = new Runnable()
+			{
+				public void run()
+				{
+					for(int i=1;i<=5;i++) {
+						System.out.println("java");
+						try {
+							Thread.sleep(500);
+						}
+						catch(Exception e) {
+							System.out.println(e);
+						}
+					}
+				}
+				
+			};
+			Runnable r1 = new Runnable() {
+				public void run() {
+					for(int i=1;i<=5;i++) {
+						System.out.println("junit");
+						try {
+							Thread.sleep(500);
+						}
+						catch(Exception e) {
+							System.out.println(e);
+						}
+					}
+				}
+			};
+				Thread t = new Thread(r);
+				t.start();
+				Thread t1 = new Thread(r1);
+				t1.start();
+		}
+
+	}
+
+
+
