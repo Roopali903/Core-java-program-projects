5d063446bdc44abbd50df116d21687389bc25667
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-08-20 00:01:45 +0530

    Add files via upload

---
+++ Assignment/java/oopsConcept/javacharacter.java
@@ -0,0 +1,19 @@
+
+public class javacharacter {
+	public static void main(String[] args) {
+  String s = "This is a program";
+ 
+  int count = 0 ;
+  
+  for( int i = 0 ; i<s.length() ; i++) {
+	  
+	  if(s.charAt(i)!=' '){
+		  count++;;
+	  }
+	  
+  }
+  System.out.println(count);
+
+	}
+}
+		// TODO Auto-generated method stub
