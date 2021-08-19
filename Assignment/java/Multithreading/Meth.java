936671f037a1126f972d3c5897b73154a496c829
Roopali chaurasia <86770933+Roopali903@users.noreply.github.com>
2021-08-20 00:21:50 +0530

    Add files via upload

---
+++ Assignment/java/Multithreading/Meth.java
@@ -0,0 +1,43 @@
+public class Meth {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Thread t = new Thread(() -> {
+			for(int i=1;i<=15;i++) {
+				System.out.println(i*i);
+				try {
+					Thread.sleep(500);
+				}
+					catch(Exception e) {
+						System.out.println(e);
+					}
+				}
+			
+		});
+		Thread t1 = new Thread(() -> {
+			for(int i=1;i<=10;i++) {
+				System.out.println("SQuare of number");
+				try {
+					Thread.sleep(500);
+				}
+					catch(Exception e) {
+						System.out.println(e);
+					}
+				}
+			
+		}
+		);
+		t.start();
+		try {
+			Thread.sleep(200);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		t1.start();
+		t.join();
+		t1.join();
+		System.out.println(" By using Thread ");
+	}
+}
+		
